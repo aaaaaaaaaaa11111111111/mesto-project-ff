4c74b6b8bf2ca0531fa6fac70b43c55958c3e203
akiyama <daniilsav2000@yandex.ru>
2024-09-05 00:21:44 +0400

    card append added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+//DOM узлы
+const cardContainer = document.querySelector('.places__list');
+//Функция создания карточки
+function createCard (name, link, deleteCard) {
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardTitle = cardItem.querySelector('.card__title');
+    cardTitle.textContent = name;
+    cardItem.querySelector('.card__image').setAttribute('src', link);
+    cardItem.querySelector('.card__image').setAttribute('alt', name);
+    cardItem.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardItem));
 
-// @todo: DOM узлы
+    return cardItem;
+};
+//Функция удаления карточки
+function deleteCard (cardItem) {
+    cardItem.remove();
+};
+//карточки на страницу
+function appendCards (cardContainer, cardItem) {
+    cardContainer.append(cardItem);
+};
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(el) {
+    appendCards(cardContainer, createCard(el.name, el.link, deleteCard));
+});
