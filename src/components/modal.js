c9bfbeb458571afd9120901c8b03ccfaee12f707
akiyama <daniilsav2000@yandex.ru>
2024-09-26 23:21:34 +0400

    modal functions and some popups

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,6 +3,25 @@ export function openModal (popup) {
     setTimeout( function () {
         popup.classList.add('popup_is-opened');
     }, 1);
+    document.addEventListener('keydown', modalCloseEsc);
+    popup.addEventListener('click', modalCloseOverlay);
 };
 
-    
+export function closeModal (popup) {
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', modalCloseEsc);
+    popup.removeEventListener('click', modalCloseOverlay);
+}
+
+function modalCloseEsc (evt) {
+    if (evt.key === 'Escape') {
+        const activeModal = document.querySelector('.popup_is-opened');
+        closeModal(activeModal)
+    }
+}
+
+function modalCloseOverlay (evt) {
+    if (evt.target === evt.currentTarget) {
+      closeModal(evt.currentTarget);
+    };
+  };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './pages/index.css';
 import { initialCards } from './components/cards.js';
 import { createCard, deleteCard } from './components/card.js'
-import { openModal } from './components/modal.js';
+import { openModal, closeModal } from './components/modal.js';
 
 const cardContainer = document.querySelector('.places__list');
 
@@ -13,8 +13,28 @@ initialCards.forEach(function(el) {
     appendCards(cardContainer, createCard(el.name, el.link, deleteCard));
 });
 
-const editButton = document.querySelector('.profile__edit-button');
-const popupEditModal = document.querySelector('.popup_type_edit')
-editButton.addEventListener('click', () => {
-    openModal(popupEditModal)
+const editButton = document.querySelector('.profile__edit-button'); 
+const popupEdit = document.querySelector('.popup_type_edit'); 
+const profileName = document.querySelector('.profile__title'); 
+const profileDescription = document.querySelector('.profile__description'); 
+const popupNameInput = document.querySelector('.popup__input_type_name'); 
+const popupDescriptionInput = document.querySelector('.popup__input_type_description'); 
+editButton.addEventListener('click', () => { 
+    popupNameInput.value = profileName.textContent; 
+    popupDescriptionInput.value = profileDescription.textContent; 
+    openModal(popupEdit); 
+}); 
+ 
+const addButton = document.querySelector('.profile__add-button'); 
+const popupAdd = document.querySelector('.popup_type_new-card'); 
+addButton.addEventListener('click', () => { 
+    openModal(popupAdd); 
+}); 
+
+const closePopupButton = document.querySelectorAll('.popup__close'); 
+closePopupButton.forEach(el => { 
+    el.addEventListener('click', () => { 
+        closeModal(popupEdit); 
+        closeModal(popupAdd);   
+    }); 
 });
